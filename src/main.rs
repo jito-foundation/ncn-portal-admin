034e8e0b9a872a239deb051170037a17f1f8d35a
Aoi <ht.so.ik0102@gmail.com>
2021-03-22 07:41:32 +0700

    create mine function

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
 use blockchainlib::*;
 
 fn main() {
-    let mut block = Block::new(0, 0, vec![0; 32], 0, "Genesis Block".to_owned());
+    let mut block = Block::new(0, 0, vec![0; 32], 0, "Genesis Block".to_owned(), 0x00fffffffffffffffffffffffffffff);
+
+    block.hash = block.hash();
+
+
 
     println!("{:?}", &block);
 
-    let h = block.hash();
-
-    println!("{:?}", &h);
-
-    block.hash = h;
+    block.mine();
 
     println!("{:?}", &block);
 }
