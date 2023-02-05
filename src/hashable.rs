fc2b311348929a18a0a2759061d32b18e2d23c0d
Aoi1011 <ht.so.ik0102@gmail.com>
2023-02-05 09:00:37 +0700

    Remove old code

--- src/hashable.rs
+++
@@ -1,9 +0,0 @@
-use super::BlockHash;
-
-pub trait Hashable {
-    fn bytes(&self) -> Vec<u8>;
-
-    fn hash(&self) -> BlockHash{
-        crypto_hash::digest(crypto_hash::Algorithm::SHA256, &self.bytes())
-    }
-}
