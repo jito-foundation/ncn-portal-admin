3cb872c1c3ec7049ad3a4328ef92c483bd54fe95
Aoi1011 <ht.so.ik0102@gmail.com>
2023-03-15 08:19:44 +0900

    Modify

--- pos/tx.go
+++ pos/tx.go
@@ -2,12 +2,13 @@ package pos
 
 import (
 	"bytes"
+	"crypto/sha256"
 	"encoding/gob"
 	"fmt"
 	"log"
 )
 
-	const subsidy = 10
+const subsidy = 10
 
 type Transaction struct {
 	From      Address
@@ -33,17 +34,28 @@ func (tx Transaction) Serialize() []byte {
 	return encoded.Bytes()
 }
 
+func (tx *Transaction) Hash() []byte {
+	var hash [32]byte
+
+	txCopy := *tx
+
+	hash = sha256.Sum256(txCopy.Serialize())
+
+	return hash[:]
+
+}
+
 func NewCoinbaseTx(data string, to Address) *Transaction {
 	if data == "" {
 		data = fmt.Sprintf("Reward to %s", to.String())
-	}	
+	}
 
-	tx := Transaction {
-		From: Address{},
-		To: to,
-		Amount: subsidy,
+	tx := Transaction{
+		From:      Address{},
+		To:        to,
+		Amount:    subsidy,
 		Signature: []byte{},
-		hash: []byte{},
+		hash:      []byte{},
 	}
 
 	return &tx
