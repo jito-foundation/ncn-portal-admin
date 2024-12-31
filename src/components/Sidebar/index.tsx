29d321783d17054f3d40885cc55efe6f78cda4ec
aoikurokawa <ht.so.ik0102@gmail.com>
2024-12-31 09:55:39 +0900

    fix: format

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -358,7 +358,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
       >
         {/* <!-- SIDEBAR HEADER --> */}
         <div className="flex items-center justify-between gap-2 px-6 py-5.5 lg:py-6.5">
-          <Link href="/">
+          {/* <Link href="/">
             <Image
               width={176}
               height={32}
@@ -366,8 +366,8 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
               alt="Logo"
               priority
             />
-          </Link>
-
+          </Link> */}
+          <h1 className="text-2xl font-bold">NCN Portal Admin</h1>
           <button
             onClick={() => setSidebarOpen(!sidebarOpen)}
             aria-controls="sidebar"
