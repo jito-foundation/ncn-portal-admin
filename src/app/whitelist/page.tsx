11f86aac1716676acde05779fa011b2b035f5615
aoikurokawa <ht.so.ik0102@gmail.com>
2025-01-04 14:24:44 +0900

    feat: upload merkle root

--- src/app/whitelist/page.tsx
+++ src/app/whitelist/page.tsx
@@ -4,6 +4,7 @@ import { getServerSession } from "next-auth";
 
 import DefaultLayout from "@/components/Layouts/DefaultLayout";
 import WhitelistTable from "@/components/Tables/WhitelistTable";
+import UploadMerkleRootButton from "@/components/Button/UploadMerkleRootButton";
 
 export const metadata: Metadata = {
   title: "Whitelist",
@@ -16,12 +17,12 @@ const WhitelistPage = async () => {
   if (!session || !session.user) {
     redirect("/api/auth/signin");
   }
-
   return (
     <DefaultLayout>
       <div className="flex flex-col gap-10">
         <div className="flex items-center justify-between">
           <h1 className="text-2xl font-semibold">Whitelist</h1>
+          <UploadMerkleRootButton />
           <a
             href="/whitelist/create"
             className="rounded bg-blue-600 px-4 py-2 text-white hover:bg-blue-700"
