f5735c0b32c091d9608d0a72d90fa7b175cab22b
aoikurokawa <ht.so.ik0102@gmail.com>
2025-01-04 14:29:36 +0900

    chore: format

--- src/app/api/whitelist/route.ts
+++ src/app/api/whitelist/route.ts
@@ -54,7 +54,7 @@ export async function POST(req: Request) {
     switch (action) {
       case "addUser":
         return addUser(req);
-    
+
       case "updateMerkleRoot":
         return updateMerkleRoot(req);
 
